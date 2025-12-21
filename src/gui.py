7606f45ae65c66fa86ad033ec7026cc40362f7cd
PS <piotrsolarz@student.agh.edu.pl>
2025-12-21 16:25:32 +0100

    cleanup code

--- src/gui.py
+++ src/gui.py
@@ -1,9 +1,10 @@
 import tkinter as tk
 from math import ceil
 from tkinter import messagebox
-from matplotlib.figure import Figure
+
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
-NavigationToolbar2Tk)
+                                               NavigationToolbar2Tk)
+from matplotlib.figure import Figure
 
 FONT_SIZE = 20
 FONT = "Helvetica"
@@ -23,43 +24,45 @@ class GUI:
 
         # init window
         self.root = tk.Tk()
-        self.update_window_params()
-
-        self.label = tk.Label()
 
         # menubar
         self.menu_bar = tk.Menu(self.root)
         self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
-        self.create_file_menu()
         self.view_menu = tk.Menu(self.menu_bar, tearoff=0)
-        self.create_view_menu()
 
-        self.root.config(menu=self.menu_bar)
-
-        # buttons
+        # buttons, checkboxes, text elements
+        self.label = tk.Label()
         self.button = tk.Button()
         self.has_iter_limit = tk.IntVar(value=1)
         self.checkbox = tk.Checkbutton()
-        self.update_text_elements()
 
-        #graph
-        fig = Figure(figsize=(4, 4))
-        x = [i for i in range(20)]
-        y = [(i**2 - 15*i) for i in x]
+        # graph
+        self.fig = Figure(figsize=(4, 4))
+        self.canvas = FigureCanvasTkAgg()
 
-        plot1 = fig.add_subplot(111)
-        plot1.plot(x,y)
-        canvas = FigureCanvasTkAgg(fig,master=self.root)
-        canvas.get_tk_widget().place(relx=0.05, rely=0.1)
-
-        # creating the Matplotlib toolbar
-        # toolbar = NavigationToolbar2Tk(canvas,self.root)
-        # toolbar.update()
-
-        # close window handling
-        self.root.protocol("WM_DELETE_WINDOW", self.close_window)
-
+        # print results
+        self.root.protocol("WM_DELETE_WINDOW", self.close_window)  # close window handling
+        self.place_everything()
         self.root.mainloop()
+
+    def place_everything(self):
+        """
+        Places every element of the GUI
+        :return:
+        """
+        self.update_window_params()
+        self.root.config(menu=self.menu_bar)
+        self.update_text_elements()
+        self.update_graph()
+
+    def create_full_menu(self):
+        """
+        Create the full menu
+        :return:
+        """
+        # TODO: assess if function is redundant (if menu will not be expanded)
+        self.create_file_menu()
+        self.create_view_menu()
 
     def update_window_params(self):
         """
@@ -92,19 +95,43 @@ class GUI:
                                        variable=self.has_iter_limit)
         self.checkbox.place(relx=0.62, rely=0.15)
 
+    def update_graph(self, x=None, y=None):
+        """
+        Plot the graph object with updates
+        :return:
+        """
+        x = [i for i in range(20)] if x is None else x
+        y = [(i ** 2 - 15 * i) for i in x] if y is None else y
+
+        plot1 = self.fig.add_subplot(111)
+        plot1.plot(x, y)
+
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
+        self.canvas.get_tk_widget().place(relx=0.05, rely=0.1)
+
+        # creating the Matplotlib toolbar
+        toolbar = NavigationToolbar2Tk(self.canvas, self.root)
+        toolbar.update()
+
     def create_file_menu(self):
         """
         Create the file menu in the upper taskbar
         :return: None
         """
+        # load from file
         self.file_menu.add_command(label="Wczytaj miasta", command=self.load_graph)
         self.file_menu.add_command(label="Wczytaj populację", command=self.load_population)
         self.file_menu.add_separator()
+
+        # randomize
         self.file_menu.add_command(label="Wylosuj miasta", command=self.generate_graph)
         self.file_menu.add_command(label="Wylosuj populację", command=self.generate_population)
         self.file_menu.add_separator()
+
+        # save loaded
         self.file_menu.add_command(label="Zapisz miasta", command=self.save_graph)
         self.file_menu.add_command(label="Zapisz populację", command=self.save_population)
+
         self.menu_bar.add_cascade(menu=self.file_menu, label="Pliki")
 
     def create_view_menu(self):
@@ -112,12 +139,16 @@ class GUI:
         Create the visual settings menu in the upper taskbar
         :return:
         """
+        # adjust config
         self.view_menu.add_command(label="Czcionka +", command=self.increase_font)
         self.view_menu.add_command(label="Czcionka -", command=self.decrease_font)
         self.view_menu.add_command(label="Wyrównaj okno", command=self.update_window_params)
         self.view_menu.add_separator()
+
+        # config.json handling
         self.view_menu.add_command(label="Zapisz konfigurację", command=self.save_config)
         self.view_menu.add_command(label="Wczytaj konfigurację", command=self.load_config)
+
         self.menu_bar.add_cascade(menu=self.view_menu, label="Widok")
 
     def close_window(self):
@@ -131,7 +162,7 @@ class GUI:
 
     def generate_solution(self):
         """
-        Generate a random solution with loaded problem and initial population.
+        Generate a random solution with loaded problem and initial population (.py).
         :return:
         """
         # TODO: implement me!
