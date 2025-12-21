a32d9dbb5501c0f4acfabef0f7048f0885eb3e95
PS <piotrsolarz@student.agh.edu.pl>
2025-12-21 01:42:19 +0100

    init menu bar

--- src/gui.py
+++ src/gui.py
@@ -5,9 +5,26 @@ class GUI:
     Represent the main GUI window
     """
     def __init__(self):
+        # init window
         self.root = tk.Tk()
-        self.root.title("Algorytm Genetyczny PSFŚ")
+        self.root.title("Problem transportowy PSFŚ")
+        self.root.geometry("500x500")
+        label = tk.Label(self.root,text = "Algorytm Genetyczny", font=("Helvetica", 20))
+        label.pack()
+
+        # menubar
+        self.menu_bar = tk.Menu(self.root)
+
+        self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.file_menu.add_command(label="Wczytaj miasta", command=self.load_graph)
+        self.file_menu.add_separator()
+        self.menu_bar.add_cascade(menu=self.file_menu, label="Pliki")
+
+        self.root.config(menu=self.menu_bar)
+
+        # close window handling
         self.root.protocol("WM_DELETE_WINDOW",self.close_window)
+
 
 
         self.root.mainloop()
@@ -17,8 +34,16 @@ class GUI:
         Handle closing via top right corner X
         :return: None
         """
-        if messagebox.askyesno(title="Zamknij",message="Czy chcesz wyjść z aplikacji?\nZmiany mogą być niezapisane!"):
+
+        if messagebox.askyesno(title="Zamknij",message="Czy na pewno chcesz wyjść z aplikacji?\nZmiany mogą być niezapisane!"):
             self.root.destroy()
+
+    def load_graph(self):
+        """
+        Handle loading graph from  .csv
+        :return:
+        """
+        pass
 
 if __name__ == '__main__':
     GUI()
