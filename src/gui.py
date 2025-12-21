5074637fdc3051ef0e66f121ec6e58dc76e2284c
PS <piotrsolarz@student.agh.edu.pl>
2025-12-21 02:22:02 +0100

    add maxiter checkbox

--- src/gui.py
+++ src/gui.py
@@ -13,32 +13,27 @@ class GUI:
         self.root.title("Problem transportowy PSFŚ")
         self.root.geometry("500x500")
         label = tk.Label(self.root, text="Algorytm Genetyczny", font=("Helvetica", 20))
+
         label.pack()
 
         # menubar
         self.menu_bar = tk.Menu(self.root)
-
-        # file handling
         self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
         self.create_file_menu()
-
-        # # algorithm handling
-        # self.alg_menu = tk.Menu(self.menu_bar, tearoff=0)
-        # self.alg_menu.add_command(label="----")
-        # self.menu_bar.add_cascade(menu=self.alg_menu, label="Algorytm")
-
-        # visual settings
-        self.set_menu = tk.Menu(self.menu_bar, tearoff=0)
-        self.set_menu.add_command(label="Czcionka +", command=self.increase_font)
-        self.set_menu.add_command(label="Czcionka -", command=self.decrease_font)
-        self.menu_bar.add_command(label="Wyrównaj okno", command=self.normalize_window_size)
-        self.menu_bar.add_cascade(menu=self.set_menu, label="Ustawienia")
+        self.view_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.create_view_menu()
 
         self.root.config(menu=self.menu_bar)
 
-        #button
-        self.button = tk.Button(self.root,text="Wygeneruj rozwiązanie", font=("Helvetica", 16), command = self.generate_solution)
+        # button
+        self.button = tk.Button(self.root, text="Wygeneruj rozwiązanie", font=("Helvetica", 16),
+                                command=self.generate_solution)
         self.button.pack(pady=10)
+
+        # checkbox
+        self.has_iter_limit = tk.IntVar(value=1)
+        self.checkbox = tk.Checkbutton(self.root, text="Limit iteracji", font=("Helvetica", 16),variable=self.has_iter_limit)
+        self.checkbox.pack(pady=10)
 
 
         # close window handling
@@ -48,7 +43,7 @@ class GUI:
 
     def create_file_menu(self):
         """
-        Create the file menu
+        Create the file menu in the upper taskbar
         :return: None
         """
         self.file_menu.add_command(label="Wczytaj miasta", command=self.load_graph)
@@ -60,15 +55,35 @@ class GUI:
         self.file_menu.add_command(label="Zapisz miasta", command=self.save_graph)
         self.file_menu.add_command(label="Zapisz populację", command=self.save_population)
         self.menu_bar.add_cascade(menu=self.file_menu, label="Pliki")
+
+    def create_view_menu(self):
+        """
+        Create the visual settings menu in the upper taskbar
+        :return:
+        """
+        self.view_menu.add_command(label="Czcionka +", command=self.increase_font)
+        self.view_menu.add_command(label="Czcionka -", command=self.decrease_font)
+        self.view_menu.add_command(label="Wyrównaj okno", command=self.normalize_window_size)
+        self.menu_bar.add_cascade(menu=self.view_menu, label="Widok")
+
     def close_window(self):
         """
         Handle closing via top right corner X
         :return: None
         """
-
         if messagebox.askyesno(title="Zamknij",
                                message="Czy na pewno chcesz wyjść z aplikacji?\nZmiany mogą być niezapisane!"):
             self.root.destroy()
+
+    def generate_solution(self):
+        """
+        Generate a random solution with loaded problem and initial population.
+        :return:
+        """
+        if self.has_iter_limit.get() == 0:
+            print("INF")
+        else:
+            print(9999)
 
     def load_graph(self):
         """
@@ -83,7 +98,6 @@ class GUI:
         :return:
         """
         pass
-
 
     def generate_graph(self):
         """
@@ -113,11 +127,7 @@ class GUI:
         """
         pass
 
-    def generate_solution(self):
-        """
-        Generate a random solution with loaded problem and initial population.
-        :return:
-        """
+
 
     # TODO: turn into lambdas
     def increase_font(self):
@@ -128,6 +138,7 @@ class GUI:
 
     def normalize_window_size(self):
         pass
+
 
 if __name__ == '__main__':
     GUI()
